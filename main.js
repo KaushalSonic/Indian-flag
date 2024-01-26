1f998dacedf39dc470bff68d5d01144a078a40f5
Kaushal <Kaushal23@gmail.com>
2024-01-26 16:19:02 +0530

    updated

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-window
+window.alert("Press INDIA below to play the 'National Anthem'");
+
+const btn = document.getElementsByClassName("btn");
+const sound = new Audio();
+
+sound.src = "Jana-Gana-Mana-Instrumental.mp3";
+    
