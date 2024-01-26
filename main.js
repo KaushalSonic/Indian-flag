5725a1c3256e1e6e7d6846e7f3d8a4afa1287af9
Kaushal <Kaushal23@gmail.com>
2024-01-26 18:27:20 +0530

    updated main

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+// A Simple JS CODE
+
+// For alert box popup
 window.alert("Press INDIA below to play the 'National Anthem'");
 
+// Creating an object of Audio()
 const sound = new Audio();
 
+// Assigning the src value in sound object
 sound.src = "Jana-Gana-Mana-Instrumental.mp3";
     
